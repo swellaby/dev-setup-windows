917ca61c803493dccb2a968c10d28efda34fead3
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-11-09 20:34:59 -0600

    fix: last iex warning

--- src/ToolManager.ps1
+++ src/ToolManager.ps1
@@ -1,23 +1,22 @@
-function Install-Tool([string]$toolArgs)
-{
+function Install-Tool() {
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingInvokeExpression', '', Justification = 'Trusted script for execution')]
+    param (
+        [string]$toolArgs
+    )
     Invoke-Expression "cinst -y $toolArgs"
     RefreshEnv
 }
 
-function Upgrade-Tool([string]$toolArgs)
-{
+function Upgrade-Tool([string]$toolArgs) {
     choco upgrade -y $toolArgs
     RefreshEnv
 }
 
-function Tool-Exists([string]$tool)
-{
-    if (-NOT (Get-Command "$tool" -ErrorAction SilentlyContinue))
-    {
+function Tool-Exists([string]$tool) {
+    if (-NOT (Get-Command "$tool" -ErrorAction SilentlyContinue)) {
         return $False
     }
-    else
-    {
+    else {
         return $True
     }
 }
